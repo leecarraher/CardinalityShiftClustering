5d432915adef6c66cf17ce86d1d394d61fcc9cd2
lee <leecarraher@gmail.com>
2013-04-18 18:44:21 -0400

    workin mean shift

--- CardinalityShift/src/mean_shift.py
+++ CardinalityShift/src/mean_shift.py
@@ -1,118 +1,125 @@
+from numpy import random
+numberofdataPoints = 100
+numberofclusers = 3
+dimensions = 3
+def getDataPoints(part,d,clu):
+    ret = [[] for j in xrange(part*clu)]
+    for i in range(clu):
+        variance = random.random()*4
+        means = [random.random(1)*10 for b in range(d)] 
+        
+        for j in range(part):
+            p = [0]*d
+            for k in xrange(d):
+                pts = random.random()
+                p[k]= (pts*variance+means[k])[0]
+            ret[i*part+j]=p
+    return ret
 
-H  = 1; #if useKNNToGetH is false, H will be used else knn will be used to determine H
-threshold = 1e-3;
-numPoints = 300;
-k = 100; % when we use knn this is the k that is used.
-
-numClasses = 0;
-pointsToCluster2 = [];
-centroids = [];
-
-def getDataPoints(numPoints,numDimensions):
-        numClasses = randi([2,8],1) # generate a random number between 2 and 8
-        dataPoints = []
-        curNumberOfPoints = 0
-
-        randomMeans = []
-        for i in range(numClasses):
-                randomMean = randi([0,100], 1) * rand(1)
-                randomStd = randi([1,2]) * rand(1)
-                curGaussianModelPoints = randomMean + randomStd .* randn(numPoints,numDimensions)
-                dataPoints = [dataPoints;curGaussianModelPoints]
-                randomMeans = [randomMeans,randomMean]
-                pointsToCluster2(curNumberOfPoints +1 : (curNumberOfPoints+numPoints)) = i
-                curNumberOfPoints = curNumberOfPoints + numPoints
-                centroids = randomMeans
-                
-        return dataPoints
-
-def sqdist(a,b):
-        #taken from demo code of class
-        aa = sum(a.*a,1)
-        bb = sum(b.*b,1)
-        ab = a.T*b
-        return abs(repmat(aa.T,[1 size(bb,2)]) + repmat(bb,[size(aa,2) 1]) - 2*ab)
-
-def getBandWith(curDataPoint,origDataPoints,euclideanDist,useKNNToGetH):
-    if (useKNNToGetH == false):
-        bandwidth = H
-    else:
-        [sortedEuclideanDist,indexInOrig] = sort(euclideanDist)
-        bandwidth = sortedEuclideanDist(k)
-    return bandwidth
-
-def getClusters(origDataPoints,finalDataPoints):
-    [numSamples,numFeatures] = len(finalDataPoints),len(finalDataPoints[0])
-    clusterCentroids  = []
-    pointsToCluster = []
-    numClusters = 0
-
-    for i in range(numSamples):
-        closestCentroid = 0 
-        curDataPoint = finalDataPoints[i]
-
-        for j in range(numClusters):
-            distToCentroid = sqrt(sqdist(curDataPoint.T,clusterCentroids[j].T))
-            if (distToCentroid <  4 * H):
-                closestCentroid = j
-                break
-
-        if (closestCentroid > 0):
-            pointsToCluster[i] = closestCentroid
-            clusterCentroids[closestCentroid] = 0.5 * (curDataPoint + clusterCentroids[closestCentroid])
-        else:
-            numClusters = numClusters + 1
-            clusterCentroids[numClusters] = finalDataPoints[i]
-            pointsToCluster[i] = numClusters
- 
-    return clusterCentroids,pointsToCluster
-
-
-def plotPoints(clusterCentroids,pointsToCluster,origDataPoints):
-    from pylab import plot
-    numSamples,numFeatures = len(origDataPoints),len(origDataPoints[0])
-
-    if (numFeatures == 2):
-        # plot the original Data Points
-        h = figure(1);
-        #hold on;
-        for i in range(numClasses):
-            colourIndex = mod(i,sizeOfColors) + 1
-            allElemsInThisCluster = find(pointsToCluster2 == i)
-            allOrigPointsInThisCluster = origDataPoints[allElemsInThisCluster]
-            plot([ i[1] for i in allOrigPointsInThisCluster ],[i[2] for i in allOrigPointsInThisCluster])
+pts =  getDataPoints(numberofdataPoints,dimensions,numberofclusers)
+   
     
-        plot(centroids,centroids)
+def sqdistance(a,b):
+    '''
+        a is a vector
+        b is a matrix
+    '''
+    aa = 0
+    for i in xrange(len(a)):
+        aa = aa + a[i] * a[i]
+    
+    bb = [aa]*len(b)
+    
+    for i in xrange(len(b)):
+        
+    
+        for j in xrange(len(b[i])):
+            bb[i] = bb[i]+b[i][j]*b[i][j]
+        for j in xrange(len(a)):
+            bb[i] = bb[i] - 2*(a[j] * b[i][j])
+    
+    return bb
+    
+    
+def getBandwidth(curPt):
+    '''
+    return distance of kth nearest neighbor
 
 
-        h = figure(2)
-        #plot the original Data Points
-        for i in range(size(clusterCentroids,1)):
-            colourIndex = mod(i,sizeOfColors) + 1 
-            allElemsInThisCluster = filter(i,pointsToCluster)
-            allOrigPointsInThisCluster = origDataPoints[allElemsInThisCluster]
-            plot([ i[1] for i in allOrigPointsInThisCluster ],[i[2] for i in allOrigPointsInThisCluster]
 
 
+    function bandwidth = getBandWith(curDataPoint,origDataPoints,euclideanDist,useKNNToGetH)
+            if (useKNNToGetH == false)
+                    bandwidth = H;
+            else:
+                    [sortedEuclideanDist,indexInOrig] = sort(euclideanDist);
+                    bandwidth = sortedEuclideanDist(k);
+            end
+    end
+    
+    '''
+    return 1.0
+#print sqdistance([1,2,3],[[1,2,4],[3,1,2],[4,1,2],[4,1,2],[4,1,2],[4,5,7]])
 
+import copy
 from math import exp
-def doMeanShift(dataPoints,useKNNToGetH):
-    numSamples,numFeatures = len[dataPoints],len[dataPoints[0]]
-    origDataPoints = dataPoints
-    for i in range(numSamples):
-        diffBetweenIterations = 10
-        while (diffBetweenIterations > threshold):
-                curDataPoint = dataPoints[i]
-                euclideanDist = sqdist(curDataPoint.T,origDataPoints.T)
-                bandwidth = getBandWith(origDataPoints[i],origDataPoints,euclideanDist,useKNNToGetH)
-                kernelDist = [exp(-euclideanDist[i]/bandwidth[i]^2) for i in xrange(len(euclideanDist))]
-                numerator = [kernelDist[i] * origDataPoints[i] for i in xrange(len(kernelDist))]  
-                denominator = sum(kernelDist)
-                newDataPoint = [numerator[i]/denominator[i] for i in xrange(len(numerator))]
-                dataPoints[i] = newDataPoint
-                diffBetweenIterations = abs(curDataPoint - newDataPoint) 
+def doMeanShift(dataPoints):
+    '''
+        perform the mean shift operations
+    '''
 
-    clusterCentroids,pointsToClusters = getClusters(origDataPoints,dataPoints)
-    plotPoints(clusterCentroids,pointsToClusters,origDataPoints)
+    origData = copy.deepcopy(dataPoints)
     
-    return origDataPoints,dataPoints
+    threshold = 1e-3
+    newVecs = []
+    for vec in dataPoints:
+        diff = threshold+1
+        while diff > threshold:
+            d = sqdistance(vec,origData)
+            bandwidth= getBandwidth(vec)
+            kernelDist = [exp(-dd)/(bandwidth*bandwidth) for dd in d]
+            
+            numerator = [0.0]*len(vec)
+            
+            for i in xrange(len(vec)):
+                for j in xrange(len(kernelDist)):
+                    numerator[i] +=  kernelDist[j] * origData[j][i]
+            
+            denom = sum(kernelDist)
+            
+            tmp = [num/denom for num in numerator]
+            
+            diff = max([abs(tmp[i]-vec[i]) for i in xrange(len(vec)) ] )
+            vec = tmp
+        
+        newVecs.append(vec)
+        
+        print diff
+    return newVecs
+        
+V = doMeanShift(pts)
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure(1)
+ax = Axes3D(fig)
+
+x = [p[0] for p in pts]
+y = [p[1] for p in pts]
+z = [p[2] for p in pts]
+
+ax.scatter(x,y,z, marker='o')
+
+x = [p[0] for p in V]
+y = [p[1] for p in V]
+z = [p[2] for p in V]
+
+ax.scatter(x,y,z, marker='x')
+plt.show()
+
+
+
+
+
